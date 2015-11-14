2a760dc9654aa2fdd509de35d470b1a04e18caa1
Imen Harrouch <harrouch.imen@gmail.com>
2015-11-14 00:14:36 -0500

    Initial commit

---
+++ app/controllers/Website.java
@@ -0,0 +1,60 @@
+package controllers;
+
+import views.html.*;
+import models.*;
+import play.mvc.*;
+import play.data.Form;
+
+import java.util.List;
+
+public class Website extends Controller {
+
+    public Result index() {
+        List<Tool> tools = Tool.find.all();
+        return ok(views.html.cts.index.render(tools));
+    }
+
+
+
+    //public Result create(){
+        //Tool tool = Form.form(Tool.class).bindFromRequest().get();
+        //tool.save();
+        //flash ("success", "Saved new tool" + tool.title);
+        //return redirect(routes.Website.show(tool.id));
+    //}
+
+    public Result show(Long id) {
+         //Query the database for a Tool with this id
+        Tool tool = Tool.find.byId(id);
+
+         //If the tool doesn't exist, then respond with a 404.
+        if (tool == null)
+            return notFound("Not Found\n");
+        else
+            return ok(views.html.cts.show.render(tool));
+
+    }
+
+
+    public Result signup()  {
+
+        User user = Form.form(User.class).bindFromRequest().get();
+
+        user.save();
+        flash ("success", "Your account has been created successfully" + user.name);
+
+        return ok(views.html.cts.signup());
+    }
+
+
+    //public Result login(Long id) {
+      //  User user = User.find.byId(id);
+
+        //if (user == null)
+          //  return notFound("Not Found\n");
+       // else
+            //return ok(views.html.cts.index());
+    //}
+
+
+}
---
+++ app/models/Tool.java
@@ -0,0 +1,29 @@
+package models;
+
+import com.avaje.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Tool extends Model {
+
+    @Id
+    public Long id;
+
+    public String title;
+
+    public String price;
+
+    public String description;
+
+    public String owner;
+
+
+    /*public List<Tool> toolList:*/
+
+
+    public static Finder<Long, Tool> find = new Finder<Long, Tool>(Tool.class);
+
+}
